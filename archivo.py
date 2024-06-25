97eeaf38726ab39d69ccc9351025aa4143877db1
cro1231 <nicola.fuentess@duocuc.cl>
2024-06-25 16:46:09 -0400

    Add files via upload

---
+++ archivo.py
@@ -0,0 +1,28 @@
+import json
+with open("archivo" "r" )as file:
+    json.load(archivo,file,)
+
+
+with open("archivo" "w")as file:
+    json.dump(archivo,file,indent=3)
+
+with True:
+    print("1.mantenidor de usuario")
+    print("2.reporte")
+    print("3.salir")
+    opcion=int(input("ingrese una opcion"))
+    
+with True:
+    print("1.agregar_usuario")
+    print("2.editar_usuario")
+    print("3.eliminar_usuario")   
+    print("4.buscar_usuario") 
+
+
+
+
+
+
+
+
+
